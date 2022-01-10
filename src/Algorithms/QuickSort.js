c12083ba322e3f881ac3296bc1697baaf197f350
Aroop Biswal <aroopbiswal@gmail.com>
2022-01-09 22:20:15 -0800

    Update QuickSort.js

--- src/Algorithms/QuickSort.js
+++ src/Algorithms/QuickSort.js
@@ -1,43 +1,41 @@
-function quickSort(array) {
-    animations = [];
-
-
-    return quickSortHelper(array);
+function quickSort(array, start, end) {
+    if (start >= end)
+    {
+        return;
+    }
+    
+    let index = quickSortHelper(array, start, end);
 }
 
 
-function quickSortHelper(array) 
+function quickSortHelper(array, start, end) 
 {
-    
-    var pivotIndex = 0;
+    var pivotIndex = start;
     var pivot = array[pivotIndex];
-    let length = array.length;
-    if (length <= 1) 
-    { 
-        return array;
-    }
-    var leftArray = [];
-    var rightArray = [];
-    
+    // let length = array.length;
 
-    for (var i = 1; i < length; i++)
+    for (var i = start; i < end; i++)
     {
-        if(i != pivotIndex)
+        if (array[i] < pivot)
         {
-            
-            if (array[i] < pivot)
-            {
-                leftArray.push(array[i]);
-                animations.push([pivotIndex, i])
-            }
-            else
-            {
-                rightArray.push(array[i]);
-            }
+            swap(array, i, pivotIndex)
+            pivotIndex++;
+        }
+        else
+        {
+            rightArray.push(array[i]);
+            animations.push([i, ]);
         }
     }
 
     return quickSortHelper(leftArray).concat(pivot, quickSort(rightArray));
+}
+
+function swap(array, index1, index2)
+{
+    var temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
 }
 
 
