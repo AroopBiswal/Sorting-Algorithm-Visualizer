0d868d52998c88eeac9984dafca32e02db1685bc
Vincent Huang <84062739+vhcent@users.noreply.github.com>
2022-01-09 21:05:07 -0800

    Add files via upload

--- src/Algorithms/QuickSort.js
+++ src/Algorithms/QuickSort.js
@@ -1,1 +1,33 @@
+function quickSort(array) 
+{
+    let length = array.length;
+    if (length <= 1) 
+    { 
+        return array;
+    }
+    var leftArray = [];
+    var rightArray = [];
+    var pivotIndex = Math.floor(length/2);
+    var pivot = array[pivotIndex];
+    
 
+    for (var i = 0; i < length; i++)
+    {
+        if(i != pivotIndex)
+        {
+            
+            if (array[i] < pivot)
+            {
+                leftArray.push(array[i]);
+            }
+            else
+            {
+                rightArray.push(array[i]);
+            }
+        }
+    }
+
+    return quickSort(leftArray).concat(pivot, quickSort(rightArray));
+}
+
+export default quickSort;
