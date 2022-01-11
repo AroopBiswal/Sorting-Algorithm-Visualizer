66f68c5eb021df63b25c30412ff5bf8ae8f51cd6
Xiye <84062739+Xiyeh@users.noreply.github.com>
2022-01-11 14:48:46 -0800

    Organization and Speed Bar update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SortingAlgorithmVisualizer from './SortingAlgorithmVisualizer/SortingAlgorithmVisualizer';
+import App from'./App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <SortingAlgorithmVisualizer/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
