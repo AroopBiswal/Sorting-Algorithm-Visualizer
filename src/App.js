085e23af2c0955f25a4215a813221426f86c1bac
Xiye <84062739+Xiyeh@users.noreply.github.com>
2022-01-12 15:00:57 -0800

    UI update

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ import quickSort from "./Algorithms/QuickSort";
 import React from "react";
 
 export default class App extends React.Component {
-    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
     constructor(props) {
         super(props);
         this.state = {
             array: [],
             speed: { min: 1, max: 10 },
+            size: 100,
         };
     }
-    handleInputChange = (e) => {
+    handleSpeed = (e) => {
         this.setState({ speed: e.currentTarget.value });
+    };
+
+    handleSize = (e) => {
+        this.setState({ size: e.currentTarget.value });
+        this.randomizeArray();
     };
 
     randomizeArray() {
         var max = 500;
         var min = 1;
         const array = [];
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < this.state.size; i++) {
             array.push(Math.floor(Math.random() * (max - min + 1) + min));
         }
         this.setState({ array });
@@ -55,75 +59,67 @@ export default class App extends React.Component {
         //console.log("Randomized Array");
         return (
             <body>
-                <div>
+                <div className="menuContainer">
                     <div class="title">Sorting Algorithm Visualizer</div>
-                    <div class="speedPanel">
-                        <p>Speed</p>
-                        <input
-                            class="input"
-                            style={{ width: "300px" }}
-                            onInput={this.handleInputChange}
-                            type="range"
-                            min="0"
-                            value={this.state.speed}
-                            max="10"
-                            step="1"
-                        />
-                    </div>
-                    <div class="algorithmPanel">
+                    <div className="speedText">Speed</div>
+                    <input
+                        className="speedBar"
+                        class="input"
+                        style={{ width: "300px" }}
+                        onInput={this.handleSpeed}
+                        type="range"
+                        min="1"
+                        max="5"
+                        step="1"
+                        value={this.state.speed}
+                    />
+                    <div className="sizeText">Size</div>
+                    <input
+                        className="sizeBar"
+                        class="input"
+                        style={{ width: "300px" }}
+                        onInput={this.handleSize}
+                        type="range"
+                        min="10"
+                        max="100"
+                        step="1"
+                        value={this.state.size}
+                    />
+                    <div className="buttons">
                         <button
-                            class="algorithmButton"
+                            class="button"
                             onClick={() => this.randomizeArray()}
                         >
-                            Generate New Array{" "}
+                            Generate New Array
                         </button>
-                        <button
-                            class="algorithmButton"
-                            onClick={() => this.quickSort()}
-                        >
+                        <button class="button" onClick={() => this.quickSort()}>
                             Quick Sort
                         </button>
                         <button
-                            class="algorithmButton"
+                            class="button"
                             onClick={() => this.bubbleSort()}
                         >
                             Bubble Sort
                         </button>
-                        <button
-                            class="algorithmButton"
-                            onClick={() => this.mergeSort()}
-                        >
+                        <button class="button" onClick={() => this.mergeSort()}>
                             Merge Sort
                         </button>
                         <button
-                            class="algorithmButton"
+                            class="button"
                             onClick={() => this.insertionSort()}
                         >
                             Insertion Sort
                         </button>
                         <button
-                            class="algorithmButton"
+                            class="button"
                             onClick={() => this.selectionSort()}
                         >
                             Selection Sort
                         </button>
-                        <button
-                            class="algorithmButton"
-                            onClick={() => this.heapSort()}
-                        >
+                        <button class="button" onClick={() => this.heapSort()}>
                             Heap Sort
                         </button>
                     </div>
-                </div>
-                <div class="playPanel">
-                    <button onClick={() => this.randomizeArray()}>
-                        Beginning
-                    </button>
-                    <button onClick={() => this.randomizeArray()}>Back</button>
-                    <button onClick={() => this.randomizeArray()}>Pause</button>
-                    <button onClick={() => this.randomizeArray()}>Play</button>
-                    <button onClick={() => this.randomizeArray()}>Next</button>
-                    <button onClick={() => this.randomizeArray()}>End</button>
                 </div>
 
                 <div id="arrayContainer" className="arrayContainer">
